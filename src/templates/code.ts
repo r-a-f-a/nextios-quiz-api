22c3927170706447110916ecce2060f216d33935
Michel Lima <m.macedomarques@gmail.com>
2020-10-13 17:49:08 -0300

    [Verification] remove https from link on email

--- src/templates/code.ts
+++ src/templates/code.ts
@@ -193,7 +193,7 @@ export const template = `<!DOCTYPE html>
                     <table border="0" cellpadding="0" cellspacing="0">
                       <tr>
                         <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                          <a href="https://quiznextios.com.br/validate/?code={code}" target="_blank"
+                          <a href="http://quiznextios.com.br/validate/?code={code}" target="_blank"
                             style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">{code}</a>
                         </td>
                       </tr>
@@ -210,7 +210,7 @@ export const template = `<!DOCTYPE html>
             <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
               <p style="margin: 0; text-align: center;">Se isso não funcionar, copie e cole o seguinte link no seu
                 navegador:</p>
-              <p style="margin: 0; text-align: center;"><a href="https://quiznextios.com.br/validate/?code={code}"
+              <p style="margin: 0; text-align: center;"><a href="http://quiznextios.com.br/validate/?code={code}"
                   target="_blank">https://quiznextios.com.br/validate/?code={code}</a></p>
             </td>
           </tr>
